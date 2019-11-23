f5861e02ac2015d4a7931f9c7b28210ff801756e
dhanys24 <36400900+dhanys24@users.noreply.github.com>
2019-11-24 05:34:58 +0700

    Update popular_programming_language.py

--- popular_programming_language.py
+++ popular_programming_language.py
@@ -1,88 +1,91 @@
-import pandas as pd
-import zipfile
-from collections import defaultdict
-import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
 
-df_2011 = pd.read_csv("./input/2011 Stack Overflow Survey Results.csv")
-df_2011['Year'] = '2011'
-df_2012 = pd.read_csv("./input/2012 Stack Overflow Survey Results.csv")
-df_2012['Year'] = '2012'
-df_2013 = pd.read_csv("./input/2013 Stack Overflow Survey Responses.csv")
-df_2013['Year'] = '2013'
-df_2014 = pd.read_csv("./input/2014 Stack Overflow Survey Responses.csv")
-df_2014['Year'] = '2014'
-df_2015 = pd.read_csv("./input/2015 Stack Overflow Developer Survey Responses.csv")
-df_2015['Year'] = '2015'
-df_2016 = pd.read_csv("./input/2016 Stack Overflow Survey Responses.csv")
-df_2016['Year'] = '2016'
-df_2017 = pd.read_csv("./input/2017 Survey Result.csv")
-df_2017['Year'] = '2017'
-df_2018 = pd.read_csv("./input/2018 Survey Result.csv")
-df_2018['Year'] = '2018'
+# coding: utf-8
+
+# In[1]:
+
+import pandas as pd
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
+
+
+# In[2]:
+
 df_2019 = pd.read_csv("./input/2019 Survey Result.csv")
 df_2019['Year'] = '2019'
 
-df_2016 = df_2016[['Year','age_range','country','gender','job_satisfaction','salary_range','programming_ability','tech_do','tech_want']]
-df_2017 = df_2017[['Respondent','Year','WantWorkLanguage','HaveWorkedLanguage','Gender','DeveloperType','Country','CompanySize','Salary','CareerSatisfaction','JobSatisfaction']]
-df_2018 = df_2018[['Respondent','LanguageDesireNextYear','LanguageWorkedWith','Age','CareerSatisfaction','CompanySize','DevType','Gender','JobSatisfaction','Salary','ConvertedSalary','Year','Country','SelfTaughtTypes']]
-df_2019 = df_2019[['Respondent','Age','CompTotal','CareerSat','Country','DevType','Gender','LanguageDesireNextYear','LanguageWorkedWith','Year']]
 
-possible_vals = ['Python','Java','JavaScript','TypeScript','Rust','Go','PHP','C#','C','Ruby','SQL',
-                 'Kotlin','WebAssembly','Swift','Clojure','Elixir','Dart','HTML/CSS','F#','Bash/Shell/PowerShell',
-                 'Scala','C++','R','Erlang','Assembly','VBA','Objective-C']
+# In[3]:
 
-df_2016_values  = df_2016['tech_do'].value_counts().reset_index()
-df_2017_values  = df_2017['HaveWorkedLanguage'].value_counts().reset_index()
-df_2018_values  = df_2018['LanguageWorkedWith'].value_counts().reset_index()
-df_2019_values  = df_2019['LanguageWorkedWith'].value_counts().reset_index()
+df_2019_lang_used = df_2019[['Respondent','LanguageWorkedWith']]
 
-df_2016_values.rename(columns={'index':'language','tech_do': 'count'}, inplace=True)
-df_2017_values.rename(columns={ 'index':'language','HaveWorkedLanguage': 'count'},inplace=True)
-df_2018_values.rename(columns={ 'index':'language','LanguageWorkedWith': 'count'},inplace=True)
-df_2019_values.rename(columns={ 'index':'language','LanguageWorkedWith': 'count'},inplace=True)
 
-def total_count(df, col1, col2, look_for):
-    '''
-    INPUT:
-    df - the pandas dataframe you want to search
-    col1 - the column name you want to look through
-    col2 - the column you want to count values from
-    look_for - a list of strings you want to search for in each row of df[col]
-    
-    OUTPUT:
-    new_df - a dataframe of each look_for with the count of how often it shows up 
-    '''
-    new_df = defaultdict(int)
-    for val in look_for:
-        for idx in range(df.shape[0]):
-            if val in df[col1][idx]:
-                new_df[val] += int(df[col2][idx])   
-    new_df = pd.DataFrame(pd.Series(new_df)).reset_index()
-    new_df.columns = [col1, col2]
-    new_df.sort_values('count', ascending=False, inplace=True)
-    return new_df
-  
-most_popular_lang_2016 = total_count(df_2016_values, 'language', 'count', possible_vals)
-most_popular_lang_2017 = total_count(df_2017_values, 'language', 'count', possible_vals)
-most_popular_lang_2018 = total_count(df_2018_values, 'language', 'count', possible_vals)
-most_popular_lang_2019 = total_count(df_2019_values, 'language', 'count', possible_vals)
+# In[4]:
 
-most_popular_lang_2016 = most_popular_lang_2016.sort_values('count',ascending=True)
-most_popular_lang_2017 = most_popular_lang_2017.sort_values('count',ascending=True)
-most_popular_lang_2018 = most_popular_lang_2018.sort_values('count',ascending=True)
-most_popular_lang_2019 = most_popular_lang_2019.sort_values('count',ascending=True)
+def splitter(values):
+    return list(itertools.product( *[str(v).split(';') for v in values]))
+
+def expand(df):
+    tuples=list()
+    for i,row in df.iterrows():
+        tuples.extend(splitter(row))
+    return  pd.DataFrame.from_records(tuples,columns=df.columns)
+
+
+# In[5]:
+
+df_2019_lang_used = expand(df_2019_lang_used)
+
+
+# In[6]:
+
+df_2019_lang_used = pd.DataFrame(df_2019_lang_used['LanguageWorkedWith'].value_counts().reset_index())
+df_2019_lang_used.rename(columns={'index':'LanguageWorkedWith','LanguageWorkedWith':'count'}, inplace=True)
+df_2019_lang_used = df_2019_lang_used.sort_values('count', ascending=True)
+df_2019_lang_used['perc'] = (df_2019_lang_used['count']/len(df_2019[df_2019['LanguageWorkedWith'].notnull()]))*100
+
+
+# In[7]:
 
 def plot_barh(df,x_col,y_col):
     x_pos = [i for i, _ in enumerate(df[x_col])]
-    plt.figure(figsize=(8,8))
+    plt.figure(figsize=(10,10))
     plt.barh(x_pos,df[y_col])
     plt.yticks(x_pos, df[x_col])
     plt.show()
-    
-    
-plot_barh(most_popular_lang_2016,'language','count')
-plot_barh(most_popular_lang_2017,'language','count')
-plot_barh(most_popular_lang_2018,'language','count')
-plot_barh(most_popular_lang_2019,'language','count')
+
+
+# In[8]:
+
+plot_barh(df_2019_lang_used,'LanguageWorkedWith','perc')
+
+
+# ### Language Users
+
+# In[9]:
+
+df_2019_lang_user = df_2019[['Respondent','LanguageWorkedWith','DevType']]
+df_2019_lang_user = df_2019_lang_user[df_2019_lang_user['DevType'].notnull()]
+df_2019_lang_user = expand(df_2019_lang_user)
+
+
+# In[12]:
+
+df_2019_lang_user = df_2019_lang_user.groupby(['LanguageWorkedWith','DevType']).size().reset_index().rename(columns={0:'count'}).sort_values('count', ascending=False)
+
+
+# In[13]:
+
+df_2019_lang_user[df_2019_lang_user['LanguageWorkedWith']=='JavaScript']
+
+
+# In[14]:
+
+df_2019_lang_user[df_2019_lang_user['LanguageWorkedWith']=='Python']
+
+
+# In[ ]:
+
+
+
